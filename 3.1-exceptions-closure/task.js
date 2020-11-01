0a17e69e1288114ffb32c647ab302c86ad4d1631
Evgeniya Dyakova <eankoma@mail.ru>
2020-11-01 15:20:37 +0700

    first commit array-proto-hocs

---
+++ 3.1-exceptions-closure/task.js
@@ -0,0 +1,59 @@
+//---------------------------------------Задача 1-------------------------------------------------
+function parseCount(value) {
+	if (isNaN(Number.parseInt(value)) === true) {
+		throw new Error("Невалидное значение");
+	}
+	return Number.parseInt(value);
+}
+
+function validateCount(value) {
+	try { 
+		parseCount(value);
+	} catch {
+		return new Error("Невалидное значение");//`Введено неверное значение "${value}"`;
+	}
+	return parseCount(value);
+}
+// console.log(parseCount("123"));
+// console.log(parseCount(123));
+// console.log(parseCount("fs123"));
+
+//----------------------------------------Задача 2-----------------------------------------------------
+class Triangle {
+	constructor (a, b, c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		if ((a + b) <= c || (b + c) <= a || (a + c) <= b) {
+			throw new Error("Треугольник с такими сторонами не существует");
+		}
+	}
+	getPerimeter() {
+		return Number(this.a + this.b + this.c);
+	}
+	getArea() {
+		let p = this.getPerimeter() / 2;
+		return Number(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3));
+		
+	}
+}
+
+function getTriangle(a, b, c) {
+	try {
+		return new Triangle(a, b, c);
+	} catch (e) {
+		return {
+			getPerimeter() {
+				return 'Ошибка! Треугольник не существует'
+			},
+			getArea() {
+				return 'Ошибка! Треугольник не существует'
+			}
+		}
+	} 
+}
+// console.log(newTriangle.getPerimeter(a, b, c) = "Ошибка! Треугольник не существует")
+ //const newTriangle = new Triangle(3, 4, 5);
+ //console.log(newTriangle.getPerimeter(3, 4, 1))
+// console.log(newTriangle.getArea(3, 4, 5))
+//console.log(getTriangle(1, 1, 5))
